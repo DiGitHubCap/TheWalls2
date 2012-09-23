93317b22fe4f6ea3cab940115402b0023fd4b5b0
Andrew Stevanus <hoot893@gmail.com>
2012-09-22 22:33:00 -0400

    Null check world for teleports

--- src/me/Hoot215/TheWalls2/util/Teleport.java
+++ src/me/Hoot215/TheWalls2/util/Teleport.java
@@ -22,12 +22,19 @@ import me.Hoot215.TheWalls2.TheWalls2;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public class Teleport {
 	public static void teleportPlayerToLocation(Player player, Location loc) {
-		Chunk chunk = Bukkit.getServer().getWorld(TheWalls2.worldName)
-				.getChunkAt(loc);
+		World world = Bukkit.getServer().getWorld(TheWalls2.worldName);
+		if (world == null) {
+			System.out.println("[TheWalls2] ERROR: World is null while " +
+					"attempting to teleport player " + player.getName());
+			return;
+		}
+		
+		Chunk chunk = world.getChunkAt(loc);
 		if (!chunk.isLoaded()) {
 			chunk.load();
 		}
